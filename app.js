873e8f58e7a66c02eaecdf84fa508895e8bea6cc
Rashenshammika <shenwotsan03@gmail.com>
2025-03-23 09:47:59 +0530

    attempt 3 and reset method set

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-let num = Math.floor(Math.random() * 10) + 1;;
+let num = Math.floor(Math.random() * 10) + 1;
+        let attempts = 3;
         
-function checkNumber() {
-    let inputVal = parseInt(document.getElementById("numberInput").value);
-    let resultText;
-    
-    if (inputVal === num) {
-        resultText = "Valid input 😍";
-        alert("you win");
-    } else if(inputVal<num) {
-        resultText = "This is Low Number 👇";
-    } else if (inputVal>num){
-        resultText = "This is High Number 👆"
-    }
-    
-    document.getElementById("result").innerText = resultText;
-}
+        function checkNumber() {
+            if (attempts > 0) {
+                let inputVal = parseInt(document.getElementById("numberInput").value);
+                let resultText;
+                
+                if (inputVal === num) {
+                    resultText = "Valid input 😍";
+                    alert("You win!");
+                    resetGame();
+                    return;
+                } else if (inputVal < num) {
+                    resultText = "This is Low Number 👇";
+                } else {
+                    resultText = "This is High Number 👆";
+                }
+                
+                attempts--;
+                if (attempts === 0) {
+                    alert("Game over! The correct number was " + num);
+                    resetGame();
+                    return;
+                }
+                
+                document.getElementById("result").innerText = resultText + " (Attempts left: " + attempts + ")";
+                
+            }  
+        }
+        
+        function resetGame() {
+            num = Math.floor(Math.random() * 10) + 1;
+            attempts = 3;
+            document.getElementById("result").innerText = "";
+            document.getElementById("numberInput").value = "";
+        }
